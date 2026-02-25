8f4f1c91ecd1b4d60ada2615f3096eb231de2b6f
Nguyen Thanh Long <109326300+nglong14@users.noreply.github.com>
2026-02-25 15:19:55 -0500

    Implemented provider interface

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -5,3 +5,21 @@ import (
 	
 	"github.com/nglong14/llmgateway/internal/models"
 )
+
+// Provider is the interface for every provider
+type Provider interface {
+	//Name of provider
+	Name() string
+	
+	//Non-streaming chat request
+	ChatCompletion(ctx context.Context, req *models.ChatCompletionRequest) (*models.ChatCompletionResponse, error)
+	
+	//Streaming chat request, send chunks to channel
+	ChatCompletionStream(ctx context.Context, req *models.ChatCompletionRequest) (<-chan *models.StreamChunk, <-chan error)
+
+	//ListModels returns model IDs available from this provider
+	ListModels(ctx context.Context) ([]models.ModelInfo, error)
+
+	//Verify the provider is valid
+	HealthCheck(ctx context.Context) error
+}
